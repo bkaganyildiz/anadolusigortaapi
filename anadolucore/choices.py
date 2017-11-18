12a1f20f1da7c2c246841bcc15861b26f9ecf754
bkaganyildiz <bkaganyildiz@gmail.com>
2017-11-18 12:24:27 +0300

    Add enum

---
+++ anadolucore/choices.py
@@ -0,0 +1,16 @@
+from .utils import ChoiceEnum
+
+class CustomerSubType(ChoiceEnum):
+    pass
+
+
+class CustomerSubType(ChoiceEnum):
+    pass
+
+
+class CustomerSubType(ChoiceEnum):
+    pass
+
+
+class CustomerSubType(ChoiceEnum):
+    pass
---
+++ anadolucore/utils.py
@@ -0,0 +1,49 @@
+import inspect, random, time
+from enum import Enum
+
+class ChoiceEnum(Enum):
+
+    @classmethod
+    def choices(cls):
+        # get all members of the class
+        members = inspect.getmembers(cls, lambda m: not(inspect.isroutine(m)))
+        # filter down to just properties
+        props = [m for m in members if not(m[0][:2] == '__')]
+        # format into django choice tuple
+        choices = tuple([(str(p[1].value), p[0]) for p in props])
+        return choices
+
+    @classmethod
+    def randomize(cls):
+        # get all members of the class
+        members = inspect.getmembers(cls, lambda m: not (inspect.isroutine(m)))
+        # filter down to just properties
+        props = [m for m in members if not (m[0][:2] == '__')]
+        # format into django choice tuple
+        choices = tuple([(str(p[1].value), p[0]) for p in props])
+        return random.choice(choices)
+
+def strTimeProp(start, end, format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formated in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+
+def randomDate(start, end, prop):
+    return strTimeProp(start, end, '%Y/%m/%d %I:%M%p', prop)
+
+print randomDate("2008/1/12 1:30PM", "2009/1/1 4:50AM", random.random())
+
+def random_coordinate(startx, endx, starty, endy):
+    points = (str(random.uniform(startx, endx)), str(random.uniform(starty, endy)))
