3431e00ea9b98d13fce7dff1ab77556375162825
Safa Ozturk <e188178@metu.edu.tr>
2017-11-18 16:44:02 +0300

    getCorrelationMatrix getCountPlot getViolinPlot

---
+++ recommend/helper.py
@@ -0,0 +1,41 @@
+
+def getCorrelationMatrix(train_data,y_indexes,x_indexes):
+    corr = train_data.corr()
+    y_final = []
+    for index in y_indexes:
+        y_final.append(corr.columns[index])
+    plt.subplots(figsize=(25,20))
+    plt.title('Correlation of Features')
+    abc = sns.heatmap(corr[y_final].iloc[x_indexes],
+                xticklabels=corr.columns[y_indexes],
+                yticklabels= corr.columns[x_indexes],
+                annot=True,
+                 linewidths=.5,
+                 square=True,
+                 cmap="Blues")
+
+    plt.savefig('correlation.png')
+    with open("correlation.png", "rb") as f:
+        data = f.read()
+        encodedString =  data.encode("base64")
+        return encodedString
+
+
+def getCountPlot(train_data,x):
+    plt.subplots(figsize=(25,15))
+    sns.countplot(x=train_data.columns[x], data=train_data);
+    plt.savefig('count.png')
+    with open("count.png", "rb") as f:
+        data = f.read()
+        encodedString =  data.encode("base64")
+        return encodedString
+
+def getViolinPlot(train_data,x,y):
+    plt.subplots(figsize=(25,15))
+    plt.title(train_data.columns[x] + ' vs ' + train_data.columns[y])
+    sns.violinplot(x=train_data.columns[x], y=train_data.columns[y], data=train_data,split=True);
+    plt.savefig('violin.png')
+    with open("violin.png", "rb") as f:
+        data = f.read()
+        encodedString =  data.encode("base64")
+        return encodedString
