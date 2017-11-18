6704085890fd09403d0445a55844e93992ff78d7
Semih Aktas <semihsercanay@gmail.com>
2017-11-18 13:03:11 +0300

    update enum choices

--- anadolucore/choices.py
+++ anadolucore/choices.py
@@ -1,16 +1,91 @@
 from .utils import ChoiceEnum
 
-class CustomerSubType(ChoiceEnum):
-    pass
+class L0(ChoiceEnum):
+    High_Income_expensive_child = 1
+    Very_Important_Provincials = 2
+    High_status_seniors = 3
+    Affluent_senior_apartments = 4
+    Mixed_seniors_1 = 5
+    Career_and_childcare = 6
+    Dinkis_double_income_no_kids = 7
+    Middle_class_families = 8
+    Modern_complete_families = 9
+    Stable_family = 10
+    Family_starters = 11
+    Affluent_young_families = 12
+    Young_all_american_family = 13
+    Junior_cosmopolitan = 14
+    Senior_cosmopolitans = 15
+    Students_in_apartments = 16
+    Fresh_masters_in_the_city = 17
+    Single_youth = 18
+    Suburban_youth = 19
+    Etnically_diverse = 20
+    Young_urban_havenots = 21
+    Mixed_apartment_dwellers = 22
+    Young_and_rising = 23
+    Young_low_educated = 24
+    Young_seniors_in_the_city = 25
+    Own_home_elderly = 26
+    Seniors_in_apartments = 27
+    Residential_elderly = 28
+    Porchless_seniors_no_front_yard = 29
+    Religious_elderly_singles = 30
+    Low_income_catholics = 31
+    Mixed_seniors_2 = 32
+    Lower_class_large_families = 33
+    Large_family_employed_child = 34
+    Village_families = 35
+    Couples_with_teens_Married_with_children = 36
+    Mixed_small_town_dwellers = 37
+    Traditional_families = 38
+    Large_religous_families = 39
+    Large_family_farms = 40
+    Mixed_rurals = 41
 
 
-class CustomerSubType(ChoiceEnum):
-    pass
+class L1(ChoiceEnum):
+    years_20_30 = 1
+    years_30_40 = 2
+    years_40_50 = 3
+    years_50_60 = 4
+    years_60_70 = 5
+    years_70_80 = 6
+
+class L2(ChoiceEnum):
+    Successful_hedonists = 1
+    Driven_Growers = 2
+    Average_Family = 3
+    Career_Loners = 4
+    Living_well = 5
+    Cruising_Seniors = 6
+    Retired_and_Religeous = 7
+    Family_with_grown_ups = 8
+    Conservative_families = 9
+    Farmers = 10
+
+class L3(ChoiceEnum):
+    percent_0 = 0
+    percent_1_10 = 1
+    percent_11_23 = 2
+    percent_24_36 = 3
+    percent_37_49 = 4
+    percent_50_62 = 5
+    percent_63_75 = 6
+    percent_76_88 = 7
+    percent_89_99 = 8
+    percent_100 = 9
 
 
-class CustomerSubType(ChoiceEnum):
-    pass
+class L4(ChoiceEnum):
+    financial_0 = 0
+    financial_1_49 = 1
+    financial_50_99 = 2
+    financial_100_199 = 3
+    financial_200_499 = 4
+    financial_500_999 = 5
+    financial_1000_4999 = 6
+    financial_5000_9999 = 7
+    financial_10000_19999 = 8
+    financial_20000_above = 9
 
-
-class CustomerSubType(ChoiceEnum):
-    pass
