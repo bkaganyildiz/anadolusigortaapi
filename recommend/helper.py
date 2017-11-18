ed6ce4ab24687f705dfbcc691d83a82e26d6601b
Safa Ozturk <e188178@metu.edu.tr>
2017-11-18 20:05:12 +0300

    Association Rules Added

--- recommend/helper.py
+++ recommend/helper.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import base64
 import seaborn as sns
 """
+import itertools
+
 class recommend_utils():
     @staticmethod
     def getCorrelationMatrix(train_data,y_indexes,x_indexes):
@@ -59,4 +61,48 @@ class recommend_utils():
         with open("violin.png", "rb") as f:
             data = f.read()
             encodedString =  data.encode("base64")
-            return encodedString
+            return encodedString,
+
+    @staticmethod
+    def lenFinder(train_data_x, indexes):
+        if indexes == []:
+            return len(train_data_x)
+        else:
+            train_data_new = train_data_x.loc[(train_data[train_data.columns[indexes[0]]] >= 1)]
+            return lenFinder(train_data_new, indexes[1:]),
+
+    @staticmethod
+    def getCombinations(lst):
+        combs = []
+
+        for i in xrange(1, len(lst) + 1):
+            els = [list(x) for x in itertools.combinations(lst, i)]
+            combs = combs + els
+        return combs
+
+    @staticmethod
+    def associationFinder(train_data, lhs, minSupport):
+        rules = []
+        N = 4000.0
+        combs = getCombinations(lhs)
+        all_values = range(43, 85)
+        for item_left in combs:
+            divider = lenFinder(train_data, item_left) * 1.0
+            if divider != 0:
+                for item_all in all_values:
+                    if item_all in lhs:
+                        continue
+                    else:
+                        lhs_plus_rhs = item_left + [item_all]
+                        support = lenFinder(train_data, lhs_plus_rhs) / N
+                        if minSupport < support:
+                            confidence = lenFinder(train_data, lhs_plus_rhs) / divider
+                            association = {
+                                'source': item_left,
+                                'dest': item_all,
+                                'support': support,
+                                'confidence': confidence
+                            }
+                            rules.append(association)
+        sorted_rules = sorted(rules, key=lambda k: k['confidence'], reverse=True)
+        return sorted_rules
